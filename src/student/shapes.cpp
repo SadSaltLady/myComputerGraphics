d49e012c5d75f37a9c922f1ce9b663659d7f24bd
SadSaltLady <yunyizha@andrew.cmu.edu>
2021-04-03 23:57:41 -0400

    tri_mesh intersection implemented

--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -19,18 +19,63 @@ Trace Sphere::hit(const Ray& ray) const {
     // TODO (PathTracer): Task 2
     // Intersect this ray with a sphere of radius Sphere::radius centered at the origin.
 
-    // If the ray intersects the sphere twice, ret should
-    // represent the first intersection, but remember to respect
-    // ray.dist_bounds! For example, if there are two intersections,
-    // but only the _later_ one is within ray.dist_bounds, you should
-    // return that one!
+    //try to reach the quadratic equation:
+    /** slide referenced: 
+     * https://cmu-graphics.github.io/Scotty3D/pathtracer/ray_sphere_intersection
+     */
+    float a = dot(ray.dir, ray.dir); //if dir is normalized, this is just 1?
+    float b = 2.0f * dot(ray.point, ray.dir);
+    float c = dot(ray.point, ray.point) - radius*radius;
 
+    //try to solve it 
+    bool pos, neg, single; //mark if we had found a solution
+    float pos_t, neg_t, dist_pos, dist_neg;
+    Vec3 pos_sol, neg_sol, first, next;
     Trace ret;
     ret.origin = ray.point;
     ret.hit = false;       // was there an intersection?
     ret.distance = 0.0f;   // at what distance did the intersection occur?
     ret.position = Vec3{}; // where was the intersection?
     ret.normal = Vec3{};   // what was the surface normal at the intersection?
+
+    pos, neg, single = false; 
+    //pos_sol, neg_sol = 15462;
+    //situation 1: no intersection -> no real solution
+    float inside = b*b - 4*a*c; //inside the sqrt
+    if (inside >= 0.0f) {
+        pos_t = b/2 + sqrt(inside);
+        neg_t = b/2 - sqrt(inside);
+        pos, neg = true;
+        if (inside == 0) {
+            single = true;
+        }
+    } else {
+        return ret;
+    }
+    
+    pos_sol = ray.point + pos_t*ray.dir;
+    neg_sol = ray.point + neg_t*ray.dir;
+
+    dist_pos = pos_sol.norm();
+    dist_neg = neg_sol.norm();
+
+    //determine which intersection came first
+    if (dist_pos < dist_neg) {
+        first = pos_sol;
+        next = neg_sol;
+    } else {
+        first = neg_sol;
+        next = pos_sol;
+    }
+
+    //if (first)
+
+    // If the ray intersects the sphere twice, ret should
+    // represent the first intersection, but remember to respect
+    // ray.dist_bounds! For example, if there are two intersections,
+    // but only the _later_ one is within ray.dist_bounds, you should
+    // return that one!
+
     return ret;
 }
 
--- src/student/tri_mesh.cpp
+++ src/student/tri_mesh.cpp
@@ -8,11 +8,26 @@ BBox Triangle::bbox() const {
 
     // TODO (PathTracer): Task 2
     // compute the bounding box of the triangle
+    Vec3 v_0 = vertex_list[v0].position;
+    Vec3 v_1 = vertex_list[v1].position;
+    Vec3 v_2 = vertex_list[v2].position;
+
+    Vec3 min_bound = Vec3(std::min(std::min(v_0.x, v_1.x), v_2.x),
+                          std::min(std::min(v_0.y, v_1.y), v_2.y),
+                          std::min(std::min(v_0.z, v_1.z), v_2.z));
+
+    Vec3 max_bound = Vec3(std::max(std::max(v_0.x, v_1.x), v_2.x),
+                          std::max(std::max(v_0.y, v_1.y), v_2.y),
+                          std::max(std::max(v_0.z, v_1.z), v_2.z));
+
 
     // Beware of flat/zero-volume boxes! You may need to
     // account for that here, or later on in BBox::intersect
 
-    BBox box;
+    //*****************NOT TAKEN CARE OF YET!!! @TOASK: WHAT TO DO?
+    //just have an eplison 
+
+    BBox box = BBox(min_bound, max_bound);
     return box;
 }
 
@@ -28,14 +43,70 @@ Trace Triangle::hit(const Ray& ray) const {
 
     // TODO (PathTracer): Task 2
     // Intersect this ray with a triangle defined by the three above points.
+    // method: the faster one (Moller-Trumbore algorithm)
+    Vec3 e1 = v_1.position - v_0.position;
+    Vec3 e2 = v_2.position - v_0.position;
+    Vec3 S = ray.point - v_0.position;
 
-    Trace ret;
+    Trace ret; //initalize ret to be in no intersection state
     ret.origin = ray.point;
     ret.hit = false;       // was there an intersection?
     ret.distance = 0.0f;   // at what distance did the intersection occur?
     ret.position = Vec3{}; // where was the intersection?
     ret.normal = Vec3{};   // what was the surface normal at the intersection?
                            // (this should be interpolated between the three vertex normals)
+
+    //calculate for (e1 x d) dot e2
+    float denominator = dot(cross(e1, ray.dir), e2);
+    //if denominator == 0, then ray parallel to face, thus no intersection
+    //if denominator < 0, then the triangle is back facing 
+    /** if you're looking back at this, here's a helpful link:
+     * https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering
+     * -a-triangle/moller-trumbore-ray-triangle-intersection
+     */
+
+    if (denominator == 0.0f) {
+        return ret;
+    }
+
+    //construct the matrix for cramer's 
+    float m_x = -(dot(cross(S, e2), ray.dir));
+    float m_y = dot(cross(e1, ray.dir), S);
+    float m_z = -(dot(cross(S, e2), e1));
+    Vec3 m = Vec3(m_x, m_y, m_z);
+    //calcualte for {u, v, t}
+    Vec3 uvt = m / denominator;
+
+    Vec3 intersect_pos = ray.point + (uvt.z * ray.dir); //location from param ray
+    float intersect_dist = (intersect_pos - ray.point).norm();
+
+    //if collision occured outside of ray dist_bounds, it is invalid
+    if (intersect_dist < ray.dist_bounds.x || intersect_dist > ray.dist_bounds.y) {
+        return ret;
+    }
+
+    //if collision occured outside of the triangle (1-u-v > 0),  it is invalid
+    //also x and y has to be strictly between 0 & 1
+    //x and y has to be between in 0-1
+    if (uvt.x < 0.0f || uvt.x > 1.0f || uvt.y < 0.0f || uvt.y > 1.0f) {
+        return ret;
+    }
+    if (1 - uvt.x - uvt.y < 0.0f || 1 - uvt.x - uvt.y > 1.0f) {
+        return ret;
+    }
+
+    //affirm that we have a collision, now calculate for all the values needed to ret
+    ret.origin = ray.point;
+    ret.hit = true;       // yes intersection
+    ret.position = intersect_pos; 
+    ret.distance = intersect_dist;
+    //interpolated between the three vertex normals
+    ret.normal = (1 - uvt.x - uvt.y)*v_0.normal + uvt.x * v_1.normal + uvt.y * v_2.normal;
+
+    //@TOASK: How should I update the ray bounds?
+    //THOUGHT: set max to the current point we have calculated
+    //update ray bounds, setting max to current pt of intersection
+    ray.dist_bounds.y = intersect_dist;
     return ret;
 }
 
