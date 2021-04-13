860f0fb5bc248108cbed7360e96fdd6200a5d0e1
SadSaltLady <yunyizha@andrew.cmu.edu>
2021-04-13 04:34:23 -0400

    fixed bug, no idea how.

--- src/student/bbox.cpp
+++ src/student/bbox.cpp
@@ -14,12 +14,15 @@ bool BBox::hit(const Ray& ray, Vec2& times) const {
      */
 
     //does the ray pass through the box
-    Vec3 invdir = 1 / ray.dir;
+
+        Vec3 invdir = 1 / ray.dir;
     float tmin, tmax, tymin, tymax, tzmin, tzmax;
     tmin = (min.x - ray.point.x) * invdir.x;
     tmax = (max.x - ray.point.x) * invdir.x;
+    if (tmin > tmax) std::swap(tmin, tmax);
     tymin = (min.y - ray.point.y) * invdir.y;
     tymax = (max.y - ray.point.y) * invdir.y;
+    if (tymin > tymax) std::swap(tymin, tymax);
 
     if((tmin > tymax) || (tymin > tmax)) {
         return false;
@@ -51,24 +54,19 @@ bool BBox::hit(const Ray& ray, Vec2& times) const {
         return false;
     }
     */
-
-    //it intersects the box, but is the intersection in range of the ray?
-    /**
-    Vec3 min_point = ray.point + tmin*ray.dir;
-    Vec3 max_point = ray.point + tmax*ray.dir;
-    float min_dist = (min_point - ray.point).norm();
-    float max_dist = (max_point - ray.point).norm();
-    if (max_dist > ray.dist_bounds.y || min_dist < ray.dist_bounds.x)    {
-        return false;
-    }
-    */
     //feedback on the times it hit
     times.x = tmin;
     times.y = tmax;
 
-    //times.x = std::min(tmin, std::min(tymin, tzmin));
-    //times.y = std::max(tmax, std::max(tzmax, tymax));
-
+    //it intersects the box, but is the intersection in range of the ray?
+    /**
+    Vec3 min_point = ray.point + tmin*ray.dir;
+    float min_dist = (min_point - ray.point).norm();
+    if (min_dist > ray.dist_bounds.y)    {
+        return false;
+    }
+    */
     //printf("hit\n");
     return true;
+    
 }
